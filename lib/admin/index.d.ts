e89dd04d7cf3cc674453618dc266ac6278292244
NikitaCGS <nikita.osypa@codegenerationsoftware.com>
2023-03-17 18:45:04 +0200

    v1

---
+++ lib/admin/index.d.ts
@@ -0,0 +1,21 @@
+export declare enum AdminCommand {
+    Statistics = "statistics",
+    Leaderboards = "leaderboards",
+    TopBets = "leaderboards:bets",
+    Users = "users",
+    User = "user",
+    CreateUser = "users:create",
+    UpdateUser = "users:update",
+    CreatePromocodes = "promocodes:create",
+    Tag = "promocodes:tag",
+    Tags = "promocodes:tags",
+    CreateTag = "promocodes:tags:create",
+    Referrals = "referrals",
+    Giveaway = "giveaway",
+    Giveaways = "giveaways",
+    CreateGiveaway = "giveaways:create",
+    UpdateGiveaway = "giveaways:update",
+    DeleteGiveaway = "giveaways:delete",
+    Rules = "rules",
+    UpdateRules = "rules:update"
+}
